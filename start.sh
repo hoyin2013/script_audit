8bb4ebe6e83ac80e3b6eeb66cbdd11590c4d23d8
hoyin2013 <kuangfeng05@163.com>
2020-06-12 16:40:40 +0800

    init static files

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker rm -f audit && docker rmi xdjk/script_audit:v1.0
 
 docker build -t xdjk/script_audit:v1.0 . --rm
 
-docker run -tid --name audit -p 8000:8000 xdjk/script_audit:v1.0
+docker run -tid --name audit -p 80:8000 xdjk/script_audit:v1.0
