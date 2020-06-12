87971879f23330c5a6d7d7d65cd4c7e333a7c825
hoyin2013 <kuangfeng05@163.com>
2020-06-12 16:08:41 +0800

    init static files

--- audit/urls.py
+++ audit/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('index/', IndexView.as_view(), name=''),
+    path('index/', IndexView.as_view(), name=''),
 ]
--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 docker build -f Dockerfile.py3 -t xdjk/py3.8 . --rm
-docker build -t xdjk/script_audit:v1.0 . --rm
+
 docker rm -f audit && docker rmi xdjk/script_audit:v1.0
+
+docker build -t xdjk/script_audit:v1.0 . --rm
 
 docker run -tid --name audit -p 8000:8000 xdjk/script_audit:v1.0
